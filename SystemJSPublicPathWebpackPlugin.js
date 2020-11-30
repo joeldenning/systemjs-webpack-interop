78308fc6176e90048ca034e2ee7cfabfa549658c
Joel Denning <joeldenning@gmail.com>
2020-11-29 22:14:13 -0700

    Implementing SystemJSPublicPathWebpackPlugin. Resolves #12. (#14)
    * Implementing SystemJSPublicPathWebpackPlugin. Resolves #12.
    
    * Self review
    
    * Documenting Resource Query approach

---
+++ SystemJSPublicPathWebpackPlugin.js
@@ -0,0 +1,69 @@
+const webpack = require("webpack");
+
+const isWebpack5 = webpack.version && webpack.version.startsWith("5.");
+
+class SystemJSPublicPathWebpackPlugin {
+  constructor(options) {
+    this.options = options || {};
+    if (!isWebpack5 && !this.options.systemjsModuleName) {
+      throw Error(
+        `SystemJSPublicPathWebpackPlugin: When using webpack@<5, you must provide 'systemjsModuleName' as an option.`
+      );
+    }
+  }
+  apply(compiler) {
+    const additionalEntries = [];
+
+    if (isWebpack5) {
+      additionalEntries.push(
+        `systemjs-webpack-interop/auto-public-path/${this.options
+          .rootDirectoryLevel || 1}`
+      );
+    } else {
+      additionalEntries.push(
+        `systemjs-webpack-interop/resource-query-public-path?systemjsModuleName=${this.options.systemjsModuleName}`
+      );
+    }
+
+    compiler.options.entry = prependEntry(
+      compiler.options.entry,
+      additionalEntries
+    );
+  }
+}
+
+// This function was copied from https://github.com/webpack/webpack-dev-server/blob/b0161e9852cdf41730e82aa43efe7e88f44a4f9d/lib/utils/DevServerPlugin.js#L72
+function prependEntry(originalEntry, additionalEntries) {
+  if (typeof originalEntry === "function") {
+    return () =>
+      Promise.resolve(originalEntry()).then(entry =>
+        prependEntry(entry, additionalEntries)
+      );
+  }
+
+  if (typeof originalEntry === "object" && !Array.isArray(originalEntry)) {
+    /** @type {Object<string,string>} */
+    const clone = {};
+
+    Object.keys(originalEntry).forEach(key => {
+      // entry[key] should be a string here
+      const entryDescription = originalEntry[key];
+      clone[key] = prependEntry(entryDescription, additionalEntries);
+    });
+
+    return clone;
+  }
+
+  // in this case, entry is a string or an array.
+  // make sure that we do not add duplicates.
+  /** @type {Entry} */
+  const entriesClone = additionalEntries.slice(0);
+  [].concat(originalEntry).forEach(newEntry => {
+    if (!entriesClone.includes(newEntry)) {
+      entriesClone.push(newEntry);
+    }
+  });
+  return entriesClone;
+}
+
+module.exports = SystemJSPublicPathWebpackPlugin;
---
+++ resource-query-public-path.js
@@ -0,0 +1,10 @@
+import { setPublicPath } from "./public-path";
+
+const queryParts = __resourceQuery.replace(/\?/g, "").split("&");
+const queryObj = queryParts.reduce((result, queryPart) => {
+  const split = queryPart.split("=");
+  result[split[0]] = split[1];
+  return result;
+}, {});
+
+setPublicPath(queryObj.systemjsModuleName);

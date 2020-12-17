339505c8c268d53a750b9fd50e04af78413f58af
Joel Denning <joeldenning@gmail.com>
2020-12-17 15:56:20 -0700

    Fixing IE11

--- resource-query-public-path.js
+++ resource-query-public-path.js
@@ -1,10 +1,13 @@
 import { setPublicPath } from "./public-path";
 
 const queryParts = __resourceQuery.replace(/\?/g, "").split("&");
-const queryObj = queryParts.reduce((result, queryPart) => {
+const queryObj = queryParts.reduce(function(result, queryPart) {
   const split = queryPart.split("=");
   result[split[0]] = split[1];
   return result;
 }, {});
 
-setPublicPath(decodeURIComponent(queryObj.systemjsModuleName), Number(queryObj.rootDirectoryLevel));
+setPublicPath(
+  decodeURIComponent(queryObj.systemjsModuleName),
+  Number(queryObj.rootDirectoryLevel)
+);

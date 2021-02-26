e345a50bb6a54e37602e8d613c9cd5c2f5809420
Joel Denning <joeldenning@gmail.com>
2021-02-25 21:47:34 -0700

    Fix IE11

--- auto-public-path/auto-public-path.js
+++ auto-public-path/auto-public-path.js
@@ -1,17 +1,21 @@
 const resolveDirectory = require("../public-path").resolveDirectory;
 
-exports.autoPublicPath = function autoPublicPath(rootDirLevel = 1) {
+exports.autoPublicPath = function autoPublicPath(rootDirLevel) {
+  if (!rootDirLevel) {
+    rootDirLevel = 1;
+  }
+
   if (typeof __webpack_public_path__ !== "undefined") {
     if (typeof __system_context__ === "undefined") {
       throw Error(
-        `systemjs-webpack-interop requires webpack@>=5.0.0-beta.15 and output.libraryTarget set to 'system'`
+        "systemjs-webpack-interop requires webpack@>=5.0.0-beta.15 and output.libraryTarget set to 'system'"
       );
     }
 
     if (!__system_context__.meta || !__system_context__.meta.url) {
       console.error("__system_context__", __system_context__);
       throw Error(
-        `systemjs-webpack-interop was provided an unknown SystemJS context. Expected context.meta.url, but none was provided`
+        "systemjs-webpack-interop was provided an unknown SystemJS context. Expected context.meta.url, but none was provided"
       );
     }
 
